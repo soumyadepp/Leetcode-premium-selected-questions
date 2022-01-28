47cfa340fa3ac5803183c7775d08e10835e5c6cc
Soumyadeep Ghosh <67363979+soumyadepp@users.noreply.github.com>
2022-01-28 14:52:46 +0530

    Create searchInsertPosition.cpp

---
+++ Apple/searchInsertPosition.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                high = mid - 1;
+            else 
+                low = mid + 1;
+        }
+        if(nums[mid] < target)
+            return mid + 1;
+        else            
+            return mid;
+    }
+};
